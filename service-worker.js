ec1b67e5971b9304075dd6a49f45a8d4a2801f23
karwankarwankk83-alt <karwankarwankk83@gmail.com>
2026-09-02 03:34:34 +0300

    Refresh cache for mobile image picker fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ks-roadmark-v12';
+const CACHE='ks-roadmark-v13';
 const CORE=[
   './','./index.html','./styles.css','./app.js','./book-data.js',
   './gallery-data.js','./manifest.json','./icon-192.png','./icon-512.png'
